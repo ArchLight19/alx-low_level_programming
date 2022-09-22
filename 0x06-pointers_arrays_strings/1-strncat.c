c9a69763f083aef5edd409f32ae1512099036071
ArchLight19 <pascalibe2014@gmail.com>
2022-09-22 00:58:44 +0000

    first

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,31 +1,33 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates specified values from src to dest
- * @src: second string to copy from
- * @dest: string to be overwritten
- * @n: number of values to concatenate
- * Description: concatenates n number of values from src to end of dest
- * Return: pointer to dest
- **/
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
+
 {
 	int i, j;
 
 	i = 0;
-
 	while (dest[i] != '\0')
-		i++;
-
-	for (j = 0; j < n; j++)
 	{
-		dest[i] = src[j];
 		i++;
 	}
 
-	if (dest[i - 1] != '\0')
-		dest[i] = '\0';
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
